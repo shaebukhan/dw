8ce3a63d8d33110f06082948fc32b661cf923b5c
shaebukhan <97784597+shaebukhan@users.noreply.github.com>
2024-09-13 19:01:59 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,55 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cors({
-  origin: 'https://dwfrontend-eight.vercel.app', // Allow all origins. Replace '*' with specific origins if needed
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
-}));
-//auth routes
-const authRoutes = require("./routes/authRoute");
-//review routes 
-const reviewRoutes = require("./routes/reviewRoute");
-
 const cors = require("cors");
-// Config
+
+const app = express();
+
+// Load environment variables
 dotenv.config();
-// Database config
+
+// Database connection
 connectDB();
-// Middlewares 
-app.use(cors());
+
+// CORS Middleware (with proper config)
+app.use(cors({
+    origin: 'https://dwfrontend-eight.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true // Enable sending cookies
+}));
+
+// Middleware to parse URL-encoded and JSON bodies
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Cookie Parser
 app.use(cookieParser());
+
+// HTTP request logger (Morgan)
 app.use(morgan("dev"));
+
+// Serving static files for uploads
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 // API routes
 app.get("/", (req, res) => {
-     res.send('Hello World!');
+    res.send("Hello World");
 });
+
+// Auth routes
+const authRoutes = require("./routes/authRoute");
 app.use("/api/v1/auth", authRoutes);
+
+// Review routes
+const reviewRoutes = require("./routes/reviewRoute");
 app.use("/api/v1/review", reviewRoutes);
 
+// Start server on specified port
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
     console.log(`Server Running on ${PORT}`);
 });
